7f9d413ccbc49a0c77fbfd785c938f3f5e87b624
Randy West <randywest55@gmail.com>
2021-04-23 22:00:34 -0700

    Add test_game_manager stubs

---
+++ tests/test_game_manager.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class ResponseContainerTestCase(unittest.TestCase):
+    pass
+
+
+class GameContainerTestCase(unittest.TestCase):
+    pass
+
+
+class GameStoreTestCase(unittest.TestCase):
+    pass
+
+
+class GameManagerTestCase(unittest.TestCase):
+    pass
