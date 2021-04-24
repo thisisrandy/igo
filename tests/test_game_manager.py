d2020bf1901a8ddf12b11e91b4563febd84e8176
Randy West <randywest55@gmail.com>
2021-04-24 12:00:37 -0700

    Add response container tests

--- tests/test_game_manager.py
+++ tests/test_game_manager.py
@@ -1,8 +1,29 @@
+from game import Color
+from game_manager import (
+    ActionResponseContainer,
+    JoinGameResponseContainer,
+    NewGameResponseContainer,
+)
 import unittest
 
 
 class ResponseContainerTestCase(unittest.TestCase):
-    pass
+    def test_new_game(self):
+        new_game = NewGameResponseContainer({Color.white: "1234", Color.black: "5678"})
+        self.assertEqual(new_game.jsonifyable(), {"white": "1234", "black": "5678"})
+
+    def test_join_game(self):
+        join_game = JoinGameResponseContainer(True, "because")
+        self.assertEqual(
+            join_game.jsonifyable(), {"success": True, "explanation": "because"}
+        )
+
+    def test_action_response(self):
+        action_response = ActionResponseContainer(False, "jesus made me do it")
+        self.assertEqual(
+            action_response.jsonifyable(),
+            {"success": False, "explanation": "jesus made me do it"},
+        )
 
 
 class GameContainerTestCase(unittest.TestCase):
