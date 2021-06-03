f9f1a1d51f2baaa5edbf6561c000b1971c3c314d
Randy West <randywest55@gmail.com>
2021-06-03 16:30:14 -0700

    Add db manager initialization

--- db_manager.py
+++ db_manager.py
@@ -2,6 +2,7 @@ from game import ChatMessage, Color, Game
 from typing import Dict, Optional, Tuple
 from asyncinit import asyncinit
 import asyncpg
+from hashlib import sha256
 
 # TODO: handle database restarts.
 # https://github.com/MagicStack/asyncpg/issues/421 seems to indicate that
@@ -29,6 +30,64 @@ class DbManager:
         # TODO: we probably want to use a connection pool instead of a single
         # connection. look into best practices
         self._connection: asyncpg.connection.Connection = await asyncpg.connect(dsn)
+
+        # machine-id is a reboot persistent unique identifier that should not be
+        # shared externally. the following mimics sd_id128_get_machine_app_specific()
+        with open("/etc/machine-id", "rb") as r:
+            self._machine_id = sha256(r.readline().strip()).hexdigest()
+
+        # we assume that the database already exists, but not that it has been
+        # set up. create tables and indices as necessary
+        await self._connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS game (
+                id serial PRIMARY KEY,
+                data bytea NOT NULL,
+                version integer NOT NULL
+            );
+
+            CREATE TABLE IF NOT EXISTS player_key (
+                key char(10) PRIMARY KEY,
+                gameid integer REFERENCES game(id) NOT NULL,
+                color char(5) NOT NULL,
+                connected boolean NOT NULL,
+                opponent_key char(10) REFERENCES player_key(key) NOT NULL,
+                managed_by char(64)
+            );
+
+            CREATE INDEX IF NOT EXISTS player_key_managed_by_index ON player_key(managed_by);
+
+            CREATE TABLE IF NOT EXISTS chat (
+                id serial PRIMARY KEY,
+                timestamp real NOT NULL,
+                color char(5) NOT NULL,
+                message text NOT NULL,
+                game_id integer REFERENCES game(id)
+            );
+
+            CREATE INDEX IF NOT EXISTS chat_id_game_id_index ON chat(game_id, id);
+            """
+        )
+
+        # if we get restarted while a client is connected to a game, the
+        # database will still reflect that the client is connected and that we
+        # are managing their connection. it is the responsibility of each game
+        # server to clean up after itself on restart
+        #
+        # NOTE: this logic breaks down if a game server never restarts or is
+        # replaced by another machine, meaning that a game key can become
+        # orphaned without manual intervention. this could be commonplace in
+        # certain environments, where some external janitorial watcher would
+        # need to be present. for now, we are assuming that the worst that
+        # happens to any game server is an unexpected restart
+        await self._connection.execute(
+            """
+            UPDATE player_key
+            SET connected = false, managed_by = null
+            WHERE managed_by = $1
+            """,
+            self._machine_id,
+        )
 
     async def create_new_game(self) -> Tuple[bool, Optional[Dict[Color, str]]]:
         """
