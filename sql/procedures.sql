f8ba37000d3fb0c522be7c3fb5573a9591247bdc
Randy West <randywest55@gmail.com>
2021-06-10 14:11:24 -0700

    Remove redundant IN designation on params

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PROCEDURE new_game(
-  IN game_data bytea,
-  IN key_w char(10),
-  IN key_b char(10),
+  game_data bytea,
+  key_w char(10),
+  key_b char(10),
   -- provide these args to join a player to the game
-  IN player_color char(5) DEFAULT null,
-  IN managed_by char(64) DEFAULT null
+  player_color char(5) DEFAULT null,
+  managed_by char(64) DEFAULT null
 )
   LANGUAGE plpgsql
 AS
