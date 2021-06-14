a86bb078790ddd020a78c2b62126a6a89f1e691a
Randy West <randywest55@gmail.com>
2021-06-14 15:17:16 -0700

    Add test db manager + basic case

---
+++ tests/test_db_manager.py
@@ -0,0 +1,49 @@
+from typing import Dict
+from game import Color, Game
+from db_manager import DbManager
+import testing.postgresql
+import unittest
+
+
+class DbManagerTestCase(unittest.IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.postgresql.stop()
+
+    async def asyncSetUp(self):
+        self.manager: DbManager = await DbManager(self.__class__.postgresql.url(), True)
+
+    async def test_startup_cleans_orphaned_rows(self):
+        manager = self.manager
+        keys: Dict[Color, str] = await manager.write_new_game(Game(), Color.white)
+        self.assertEqual(
+            keys[Color.white],
+            await manager._connection.fetchval(
+                """
+            SELECT key
+            FROM player_key
+            WHERE managed_by = $1
+            """,
+                manager._machine_id,
+            ),
+        )
+
+        del manager
+        manager: DbManager = await DbManager(self.__class__.postgresql.url(), False)
+        # NOTE: it's important to fetch the row and not the value here, because
+        # null being the expected type, fetchval conflates "returned a row with
+        # a null value" and "didn't return any rows." we only want to succeed on
+        # the former, so we fetch a full row and then get the value from it
+        row = await manager._connection.fetchrow(
+            """
+            SELECT managed_by
+            FROM player_key
+            WHERE key = $1
+            """,
+            keys[Color.white],
+        )
+        self.assertIsNone(row.get("managed_by"))
