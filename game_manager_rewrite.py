ae66f8dadfdee7666178d75e58251f0e697d975d
Randy West <randywest55@gmail.com>
2021-06-17 17:05:23 -0700

    Add GameStore attr + flesh out new game

--- game_manager_rewrite.py
+++ game_manager_rewrite.py
@@ -3,9 +3,46 @@ Temporary file to hold rewrite of game_manager. Once fleshed out, overwrite
 game_manager and remove this file
 """
 
+from game_manager import NewGameResponseContainer
+from constants import COLOR, KOMI, SIZE
+import logging
+from chat import ChatThread
+from dataclasses import dataclass
+from game import Color, Game
+from typing import Dict, Optional
 from tornado.websocket import WebSocketHandler
-from messages import IncomingMessage, IncomingMessageType
+from messages import (
+    IncomingMessage,
+    IncomingMessageType,
+    OutgoingMessageType,
+    send_outgoing_message,
+)
 import asyncinit
+from db_manager import DbManager
+
+
+@dataclass
+class ClientData:
+    """
+    ClientData is a container for all of the various data that a single client
+    is concerned with.
+
+    Attributes:
+
+        key: str - the client's player key
+
+        game: Game - the current game
+
+        chat_thread: ChatThread - the chat thread associated with the current game
+
+        opponent_connected: bool - whether or not the client's opponent in the
+        current game is connected to a game server
+    """
+
+    key: str
+    game: Game
+    chat_thread: ChatThread
+    opponent_connected: bool
 
 
 @asyncinit
@@ -13,18 +50,57 @@ class GameStore:
     """
     GameStore is the guts of the in-memory storage and management of games. It
     maps connected clients, identified by their web socket handler, one-to-one
-    to all of the data they are concerned with, noting that a client who has not
-    yet created or joined a game will be mapped to a unique empty data set.
-    Although it is possible and indeed likely that two clients playing the same
-    game will be connected to the same game server, no attempt is made to share
-    data between them above the database level.
+    to all of the data they are concerned with. Although it is possible and
+    indeed likely that two clients playing the same game will be connected to
+    the same game server, no attempt is made to share data between them above
+    the database level.
     """
 
     async def __init__(self, store_dsn: str) -> None:
-        pass
+        self._clients: Dict[WebSocketHandler, ClientData] = {}
+        self._keys: Dict[str, WebSocketHandler] = {}
+        self._db_manager: DbManager = await DbManager(store_dsn)
 
     async def new_game(self, msg: IncomingMessage) -> None:
-        pass
+        """
+        Create and write out a new game and then respond appropriately
+        """
+
+        if msg.websocket_handler in self._clients:
+            old_key = self._clients[msg.websocket_handler].key
+            logging.info(f"Client requesting new game already subscribed to {old_key}")
+            await self.unsubscribe(msg.websocket_handler)
+
+        game = Game(msg.data[SIZE], msg.data[KOMI])
+        requested_color = Color[msg.data[COLOR]]
+        keys: Dict[Color, str] = await self._db_manager.write_new_game(
+            game, requested_color
+        )
+        client_key = keys[requested_color]
+        self._clients[msg.websocket_handler] = ClientData(
+            client_key, game, ChatThread()
+        )
+        self._keys[client_key] = msg.websocket_handler
+
+        # TODO: If msg.data[VS] is "computer", set up computer as second player
+
+        await send_outgoing_message(
+            OutgoingMessageType.new_game_response,
+            NewGameResponseContainer(
+                True,
+                (
+                    f"Successfully created new game. Make sure to give the"
+                    f" {requested_color.inverse().name} player key"
+                    f" ({keys[requested_color.inverse()]}) to your opponent so that"
+                    f" they can join the game. Your key is {keys[requested_color]}."
+                    f" Make sure to write it down in case you want to pause the game"
+                    f" and resume it later, or if you want to view it once complete"
+                ),
+                keys,
+                requested_color,
+            ),
+            msg.websocket_handler,
+        )
 
     async def join_game(self, msg: IncomingMessage) -> None:
         pass
