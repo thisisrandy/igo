7c3c3ff45f7617d8888474bb24c575578302a1f9
Randy West <randywest55@gmail.com>
2021-07-16 15:07:00 -0700

    Fill out policy base + add random policy

--- igo/aiserver/policy/base.py
+++ igo/aiserver/policy/base.py
@@ -1,3 +1,22 @@
 """
 Abstract base class which all play policies must implement
 """
+
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from asyncinit import asyncinit
+from igo.game import Action, Color, Game
+
+
+@asyncinit
+class PlayPolicyBase(ABC):
+    @abstractmethod
+    async def play(self, game: Game, color: Color) -> Optional[Action]:
+        """
+        If `color` is allowed to take an action, select one and return it.
+        Otherwise, do any bookkeeping related to the last action taken and
+        return None
+        """
+
+        raise NotImplementedError()
--- igo/aiserver/policy/mcts.py
+++ igo/aiserver/policy/mcts.py
@@ -1,4 +1,16 @@
-"""
-Go AI policy using Monte Carlo Tree Search (see
-https://en.wikipedia.org/wiki/Monte_Carlo_tree_search)
-"""
+from typing import Optional
+from igo.game import Action, Color, Game
+from igo.aiserver.policy.base import PlayPolicyBase
+
+
+class MCTSPolicy(PlayPolicyBase):
+    """
+    Go AI policy using Monte Carlo Tree Search (see
+    https://en.wikipedia.org/wiki/Monte_Carlo_tree_search)
+    """
+
+    async def __init__(self) -> None:
+        pass
+
+    async def play(self, game: Game, color: Color) -> Optional[Action]:
+        pass
