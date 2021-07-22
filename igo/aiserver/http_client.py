fae5502460bc24326abd5c3cb0e27d65467429d5
Randy West <randywest55@gmail.com>
2021-07-21 20:27:26 -0700

    Add http client

---
+++ igo/aiserver/http_client.py
@@ -0,0 +1,64 @@
+import asyncio
+import logging
+from igo.gameserver.containers import KeyPair
+from tornado.httpclient import AsyncHTTPClient, HTTPResponse
+import os
+import re
+from typing import Optional, Dict
+from asyncio import Lock
+
+SLEEP_FOR = 2
+_client = AsyncHTTPClient()
+_endpoint = f"{os.getenv('AI_SERVER_URL', 'http://localhost:1918')}/start"
+_xsrf_headers: Optional[Dict[str, str]] = None
+_xsrf_lock: Lock = Lock()
+
+__all__ = ("start_ai_player",)
+
+
+async def start_ai_player(keys: KeyPair, just_once: bool = False) -> None:
+    """
+    This is the client's method to initiate communication with the AI server. It
+    will try continuously to successfully POST to the AI server's http listener
+    unless `just_once`, in which case it will re-raise any exceptions on failure
+    """
+
+    global _xsrf_headers
+    assert keys.ai_secret, "Cannot start an AI player without their secret"
+
+    res: HTTPResponse
+    while True:
+        try:
+            if not _xsrf_headers:
+                async with _xsrf_lock:
+                    if not _xsrf_headers:
+                        res = await _client.fetch(_endpoint)
+                        xsrf = re.search(r"_xsrf=(.*?);", res.headers["Set-Cookie"])[1]
+                        _xsrf_headers = {"X-XSRFToken": xsrf, "Cookie": f"_xsrf={xsrf}"}
+
+            await _client.fetch(
+                (
+                    f"{_endpoint}?"
+                    f"player_key={keys.player_key}"
+                    f"&ai_secret={keys.ai_secret}"
+                ),
+                method="POST",
+                headers=_xsrf_headers,
+                body="",
+            )
+
+        except Exception as e:
+            if just_once:
+                raise Exception("Failed to contact the AI server") from e
+            else:
+                logging.exception(
+                    f"Failed to contact the AI server. Sleeping for {SLEEP_FOR}s and"
+                    " trying again"
+                )
+                await asyncio.sleep(SLEEP_FOR)
+
+        else:
+            logging.info(
+                f"Successfully contracted the AI server to play for key {keys.player_key}"
+            )
+            break
